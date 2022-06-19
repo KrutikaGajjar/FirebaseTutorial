504ed0e877e70f542400bb85e7f02eee8e7fe8a1
Aman Singh Tomar <aman@Amans-MacBook-Air.local>
2022-06-19 14:28:54 +0530

    Read Data part-1

---
+++ src/Components/Cards/customCard.js
@@ -0,0 +1,28 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+export default function CustomCard({ details }) {
+    const {name, address, place, time} = details
+	return (
+		<Card sx={{ width: "20%", margin: "15px" }} variant="outlined" >
+			<CardContent>
+                <Typography variant="h5" component="div">{name }</Typography>
+				<Typography sx={{ mb: 1.5 }} color="text.secondary">
+					{time}
+				</Typography>
+				<Typography variant="body2">
+					{address}
+					<br />
+					{place}
+				</Typography>
+			</CardContent>
+			<CardActions>
+				<Button size="small">Delete Event</Button>
+			</CardActions>
+		</Card>
+	);
+}
--- src/Container/Dashboard/dashboard.js
+++ src/Container/Dashboard/dashboard.js
@@ -1,17 +1,43 @@
-import { Button, Typography } from '@mui/material'
-import { onAuthStateChanged, signOut } from 'firebase/auth'
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router'
-import CustomDrawer from '../../Components/Drawer/customDrawer'
-import { auth } from '../../firebase'
+
+import { Box } from '@mui/system'
+import { collection, getDocs} from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import CustomCard from '../../Components/Cards/customCard'
+import { db } from '../../firebase'
+
+const data = [
+  { name: 'Fake Name', place: 'Fake Place', address: 'Fake address', time: 'fake time',id:'1' },
+  { name: 'Fake Name', place: 'Fake Place', address: 'Fake address', time: 'fake time',id:'2' },
+  { name: 'Fake Name', place: 'Fake Place', address: 'Fake address', time: 'fake time',id:'3' },
+  { name: 'Fake Name', place: 'Fake Place', address: 'Fake address', time: 'fake time',id:'4' },
+  { name: 'Fake Name', place: 'Fake Place', address: 'Fake address', time: 'fake time',id:'5' },
+  { name: 'Fake Name', place: 'Fake Place', address: 'Fake address', time: 'fake time',id:'6' },
+]
 
 export default function Dashboard() {
 
+  const [eventData, setEventData] = useState(data)
+
+  useEffect(() => {
+    const collRef = collection(db, 'Events');
+    let data=[]
+    getDocs(collRef).then((querySnapshot) => {
+       querySnapshot.forEach(query => {
+        console.log("query is", query.data(), query.id)
+        data.push({...query.data(),id:query.id})
+      })
+      console.log("data",data)
+      setEventData(data);
+    })
+  },[])
+
     return (
       <>
-       
-        <Typography variant='h3'>Welcome to Dashboard</Typography>
-        
+        <Box sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', flexFlow: 'wrap' }}>
+          {eventData.map(detail => {
+            return <CustomCard details={detail} key={detail.id}/>
+          })}
+        </Box>   
       </>
   )
 }
